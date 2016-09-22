36310c12abac4325393a6d62e121a6e93393fa87
Allison.Cuyjet <allison@cuyjet.com>
2016-09-21 20:05:33 -0400

    Added work from the Parsing Data section of the C# Streams and Data Processing course

--- SoccerStats/SoccerStats/GameResult.cs
+++ SoccerStats/SoccerStats/GameResult.cs
@@ -9,5 +9,22 @@ namespace SoccerStats
     public class GameResult
     {
         public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeOrAway { get; set; }
+        public int Goals { get; set; }
+        public int GoalAttempts { get; set; }
+        public int ShotsOnGoal { get; set; }
+        public int ShotsOffGoal { get; set; }
+        public double PosessionPercent { get; set; }
+        public double ConversionRate { get
+            {
+                return (double)Goals / (double)GoalAttempts;
+            }
+        }
+    }
+    public enum HomeOrAway
+    {
+        Home,
+        Away
     }
 }
--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -25,9 +25,9 @@ namespace SoccerStats
             }
         }
 
-        public static List<string[]> ReadSoccerResults(string fileName)
+        public static List<GameResult> ReadSoccerResults(string fileName)
         {
-            var soccerResults = new List<string[]>();
+            var soccerResults = new List<GameResult>();
             using (var reader = new StreamReader(fileName))
             {
                 string line = "";
@@ -36,13 +36,43 @@ namespace SoccerStats
                 {
                     var gameResult = new GameResult();
                     string[] values = line.Split(',');
+                    int parseInt;
+                    double posessionPercent;
 
                     DateTime gameDate;
                     if (DateTime.TryParse(values[0], out gameDate))
                     {
                         gameResult.GameDate = gameDate;
                     }
-                    soccerResults.Add(values);
+                    gameResult.TeamName = values[1];
+
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+                    if (double.TryParse(values[7], out posessionPercent))
+                    {
+                        gameResult.PosessionPercent = posessionPercent;
+                    }
+                    soccerResults.Add(gameResult);
                 }
             }
             return soccerResults;
