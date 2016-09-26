65ec0e4efcaf099e91b29efc841b66e62ce872e6
Allison.Cuyjet <allison@cuyjet.com>
2016-09-26 18:55:52 -0400

    Added Comic Book Detail view

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the ComicBooksController!");
+            return View();
         }
     }
 }
