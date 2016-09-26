41cf1f0aacbb968ba030b82eb5135d02c28929d3
Allison.Cuyjet <allison@cuyjet.com>
2016-09-26 19:39:54 -0400

    Updated Comic Book Detail view

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,19 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Saga";
+            ViewBag.IssueNumber = 38;
+            ViewBag.Description = "<p>\"THE WAR FOR PHANG,\" Part Two. When your babysitter's a ghost, death is never very far away.</p>";
+            ViewBag.Credits = new string[]
+            {
+                "Script: Brian K. Vaughan",
+                "Pencils: Fiona Staples",
+                "Inks: Fiona Staples",
+                "Colors: Fiona Staples",
+                "Letters: Fonografiks",
+                "Cover: Fiona Staples"
+            };
+
             return View();
         }
     }
