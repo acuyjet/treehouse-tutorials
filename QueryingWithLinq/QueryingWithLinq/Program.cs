78712a571f1351a4aed1d129a422971a20364ecb
Allison.Cuyjet <allison@cuyjet.com>
2016-10-04 12:12:02 -0400

    Added work from the Functional Programming in C# section of the Querying with LINQ course

---
+++ QueryingWithLinq/QueryingWithLinq/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryingWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> sayGreeting;
+
+            Func<string, string> converse = (message) =>
+            {
+                Console.WriteLine(message);
+                return Console.ReadLine();
+            };
+
+            string input = converse("What's your name?");
+
+            sayGreeting = (greeting) =>
+            {
+                Console.WriteLine(string.Format(greeting, input));
+            };
+
+            sayGreeting("Hello, {0}");
+            converse("Nice to see you.");
+            converse("Say hi to your mother for me.");
+            sayGreeting("Goodbye, {0}");
+        }
+    }
+}
