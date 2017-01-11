72f33ae105722d7aafc83df0c43785a49c6ee229
Allison.Cuyjet <allison@cuyjet.com>
2017-01-11 17:04:00 -0500

    Add work from Javascript and the DOM course

---
+++ JavascriptAndTheDOM/app.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+
+myButton.addEventListener('click', () => {
+    myHeading.style.color = myTextInput.value;
+});
+
