8ad6e5e91f8f4129254353331d83743c09d70fb3
Allison.Cuyjet <allison@cuyjet.com>
2017-01-12 16:23:28 -0500

    Add work from the 'Getting a Handle on the DOM' section of the JavaScript and the DOM course

--- JavascriptAndTheDOM/app.js
+++ JavascriptAndTheDOM/app.js
@@ -1,9 +1,28 @@
 // JavaScript source code
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
 
-myButton.addEventListener('click', () => {
-    myHeading.style.color = myTextInput.value;
-});
+//const myHeading = document.getElementsByTagName('h1')[0];
+//const myButton = document.getElementById('myButton');
+//const myTextInput = document.getElementById('myTextInput');
 
+//myButton.addEventListener('click', () => {
+//    myHeading.style.color = myTextInput.value;
+//});
+
+const myList = document.getElementsByTagName('li');
+
+for (let i = 0; i < myList.length; i++) {
+    myList[i].style.color = 'purple';
+}
+
+//const errorNotPurple = document.getElementsByClassName('error-not-purple');
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i++) {
+    errorNotPurple[i].style.color = 'red';
+}
+
+const evens = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < evens.length; i++) {
+    evens[i].style.backgroundColor = 'lightgray';
+}
