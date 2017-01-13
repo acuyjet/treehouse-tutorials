dc482c92fc18dee2438848118b6c9425316bcf0a
Allison.Cuyjet <allison@cuyjet.com>
2017-01-13 13:49:03 -0500

    Add work from the 'Making changes to the DOM' section of the JavaScript and the DOM course

--- JavascriptAndTheDOM/app.js
+++ JavascriptAndTheDOM/app.js
@@ -1,10 +1,40 @@
-const input = document.querySelector('input');
-const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
 
-button.addEventListener('click', () => {
-    p.innerHTML = input.value + ':';
+toggleList.addEventListener('click', () => {
+    if (listDiv.style.display == 'none') {
+        toggleList.textContent = 'Hide List';
+        listDiv.style.display = 'block';
+    } else {
+        toggleList.textContent = 'Show List';
+        listDiv.style.display = 'none';
+    }
+});
+
+descriptionButton.addEventListener('click', () => {
+    descriptionP.innerHTML = descriptionInput.value + ':';
+    descriptionInput.value = '';
 });
 
 //p.title = "List discription";
+
+addItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = document.createElement('li');
+    li.textContent = addItemInput.value;
+    ul.appendChild(li);
+    addItemInput.value = '';
+});
+
+removeItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = document.querySelector('li:last-child');
+    ul.removeChild(li);
+});
 
