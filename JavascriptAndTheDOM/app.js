e511449213bbb1794750c05830f7770fcbc48349
acuyjet <allison@cuyjet.com>
2017-01-15 17:04:51 -0500

    Add work from the 'Responding to User Interaction' section of the Javascript and the DOM course

--- JavascriptAndTheDOM/app.js
+++ JavascriptAndTheDOM/app.js
@@ -6,16 +6,22 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
-const listItems = document.getElementsByTagName('li');
 
-for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener('mouseover', () => {
-        listItems[i].textContent = listItems[i].textContent.toUpperCase();
-        listItems[i].addEventListener('mouseout', () => {
-            listItems[i].textContent = listItems[i].textContent.toLowerCase();
-        });
-    });
-}
+listDiv.addEventListener('mouseover', (event) => {
+    if (event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toUpperCase();
+    }
+});
+
+listDiv.addEventListener('mouseout', (event) => {
+    if (event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toLowerCase();
+    }
+});
+
+//document.addEventListener('click', (event) => {
+//    console.log(event.target);
+//})
 
 toggleList.addEventListener('click', () => {
     if (listDiv.style.display == 'none') {
