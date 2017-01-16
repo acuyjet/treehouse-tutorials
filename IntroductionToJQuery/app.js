1ed605daecec634abd162abf240323d3db538c7a
acuyjet <allison@cuyjet.com>
2017-01-15 21:06:52 -0500

    Add work from Introduction to jQuery course

--- IntroductionToJQuery/app.js
+++ IntroductionToJQuery/app.js
@@ -1,3 +1,19 @@
-$('li').on('click', function() {
-	$(this).text("Clicked!");
+$('li').on('click', function () {
+    $(this).text("Clicked!").delay(800).fadeOut(400);
 });
+
+//Element selectors
+//  $('li');
+//  $('a');
+//  $('img');
+//  $('p');
+
+//Descendant selectors
+//  $('p a');
+//    
+//Classes and ids
+//  $('.blog-post');
+//  $('#about');
+//    
+//Attribute selectors
+//  $("[name='newsletter']");
