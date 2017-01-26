ad2ffe1718f605251424823710fd87677658c6b3
acuyjet <allison@cuyjet.com>
2017-01-25 21:12:14 -0500

    Add some work from the 'Building a Command Line Application' section of the Node.js Basics course

--- Node.js Basics/app.js
+++ Node.js Basics/app.js
@@ -1,3 +1,7 @@
-console.log('Hello, world!');
-console.error("You done messed up!");
-console.dir({name: "Allison", age: 37});
+// Problem:  We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+
+// Connect to the API URL (https://teamtreehouse.com/username.json)
+// Read the data
+// Parse the data
+// Print the data
