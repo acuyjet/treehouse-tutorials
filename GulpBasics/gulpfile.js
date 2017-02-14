3fda64509946b793e9570728f1c5dd977f848a5d
acuyjet <allison@cuyjet.com>
2017-02-13 20:58:45 -0500

    Add work from the second stage of the Gulp Basics course

--- GulpBasics/gulpfile.js
+++ GulpBasics/gulpfile.js
@@ -1,9 +1,24 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
-gulp.task('hello', function(){
-    console.log('Hello!');
+gulp.task('concat-scripts', function(){
+    gulp.src([
+        'js/jquery.js',
+        'js/sticky/jquery.sticky.js',
+        'js/main.js'])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('minify-scripts', function(){
+    gulp.src('js/app.js')
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('js'));
 });
 
 gulp.task('default', ['hello'], function(){
