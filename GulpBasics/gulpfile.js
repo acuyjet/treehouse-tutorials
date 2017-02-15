da243d19c6f58012f14257984f4a57ee877c27aa
acuyjet <allison@cuyjet.com>
2017-02-14 19:02:56 -0500

    Add work from the third stage of the Gulp Basics course

--- GulpBasics/gulpfile.js
+++ GulpBasics/gulpfile.js
@@ -3,14 +3,18 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps');
 
 gulp.task('concat-scripts', function(){
     gulp.src([
         'js/jquery.js',
         'js/sticky/jquery.sticky.js',
         'js/main.js'])
+        .pipe(maps.init())
         .pipe(concat('app.js'))
+        .pipe(maps.write('./'))
         .pipe(gulp.dest('js'));
 });
 
@@ -19,6 +23,14 @@ gulp.task('minify-scripts', function(){
         .pipe(uglify())
         .pipe(rename('app.min.js'))
         .pipe(gulp.dest('js'));
+});
+
+gulp.task('compile-sass', function(){
+   gulp.src('scss/application.scss')
+       .pipe(maps.init())
+       .pipe(sass())
+       .pipe(maps.write('./'))
+       .pipe(gulp.dest('css'));
 });
 
 gulp.task('default', ['hello'], function(){
