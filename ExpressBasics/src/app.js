dffe555cf8527bba76f7e6a36417a1bce2f82c30
acuyjet <allison@cuyjet.com>
2017-02-19 18:17:06 -0500

    Add work from second stage of Express Basics course

--- ExpressBasics/src/app.js
+++ ExpressBasics/src/app.js
@@ -1,12 +1,19 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    posts = require('./mock/posts.json');
 
 var app = express();
+
+debugger;
 
 // Create a route
 app.get('/', function(req, res){
     res.send('<h1>Hello, world!</h1>');
+});
+
+app.get('/blog', function(req, res) {
+    res.send(posts);
 });
 
 app.listen(3000, function(){
