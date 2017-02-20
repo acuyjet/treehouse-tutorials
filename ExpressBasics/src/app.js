8e57fc7d9298dc023c88a7485a42072e2c4171d1
acuyjet <allison@cuyjet.com>
2017-02-19 19:03:41 -0500

    Add work from third stage of Express Basics course

--- ExpressBasics/src/app.js
+++ ExpressBasics/src/app.js
@@ -5,15 +5,22 @@ var express = require('express'),
 
 var app = express();
 
-debugger;
-
-// Create a route
+// Create an index route
 app.get('/', function(req, res){
     res.send('<h1>Hello, world!</h1>');
 });
 
-app.get('/blog', function(req, res) {
-    res.send(posts);
+// "?" indicates that the parameter is optional
+app.get('/blog/:title?', function(req, res) {
+    var title = req.params.title;
+    if(title === undefined) {
+        res.status(503);
+        res.send('This page is under construction');
+    } else {
+        var post = posts[title];
+        res.send(post);
+    }
+
 });
 
 app.listen(3000, function(){
