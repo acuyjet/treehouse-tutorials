199e1b2e5f86104db9d246325fdfdfc13b2aef80
acuyjet <allison@cuyjet.com>
2017-02-19 21:02:46 -0500

    Add work from fourth stage of Express Basics course

--- ExpressBasics/src/app.js
+++ ExpressBasics/src/app.js
@@ -5,9 +5,14 @@ var express = require('express'),
 
 var app = express();
 
+app.set('view engine', 'pug');
+
+// "__dirname" variable gives path to current file
+app.set('views', __dirname + '/views');
+
 // Create an index route
 app.get('/', function(req, res){
-    res.send('<h1>Hello, world!</h1>');
+    res.render('index');
 });
 
 // "?" indicates that the parameter is optional
@@ -17,10 +22,9 @@ app.get('/blog/:title?', function(req, res) {
         res.status(503);
         res.send('This page is under construction');
     } else {
-        var post = posts[title];
-        res.send(post);
+        var post = posts[title] || {};
+        res.render('post', {post: post});
     }
-
 });
 
 app.listen(3000, function(){
