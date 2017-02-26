616008466dc9430a92e5e48cc7028e2591d2b4a0
acuyjet <allison@cuyjet.com>
2017-02-26 15:31:32 -0500

    Add work from penultimate stage of Angular JS course

--- AngularJS/scripts/app.js
+++ AngularJS/scripts/app.js
@@ -1,16 +1,46 @@
 //Empty array as second arg creates app if it doesn't already exist
 angular.module('todoListApp', [])
-    .controller('mainCtrl', function($scope) {
-        $scope.learningNgChange = function() {
-            console.log('An input changed!');
+    .controller('mainCtrl', function($scope, dataService) {
+        $scope.helloConsole = dataService.helloConsole;
+
+        $scope.addTodo = function() {
+            var todo = {name: "This is a new task!"};
+            $scope.todos.push(todo);
         };
 
-    $scope.todos = [
-        {"name" : "Clean the house"},
-        {"name" : "Water the dog"},
-        {"name" : "Feed the lawn"},
-        {"name" : "Pay dem bills"},
-        {"name" : "Run"},
-        {"name" : "Stretch"}
-    ]
-});
+        dataService.getTodos(function(response) {
+                // When a successful response is received, do something
+                console.log(response.data);
+                $scope.todos = response.data;
+            });
+
+        $scope.deleteTodo = function(todo, $index) {
+            dataService.deleteTodo(todo);
+            $scope.todos.splice($index, 1);
+        };
+
+        $scope.saveTodo = function(todo) {
+            dataService.saveTodo(todo);
+        }
+
+})
+    .service('dataService', function($http) {
+        this.helloConsole = function() {
+            console.log('This is the helloConsole service!');
+        };
+
+        this.getTodos = function(callback) {
+            $http.get('mock/todos.json')
+                .then(callback);
+        };
+
+        this.deleteTodo = function(todo) {
+            console.log('The "' + todo.name + '" task has been deleted!');
+            // Other db logic would go here
+       };
+
+        this.saveTodo = function(todo) {
+            console.log('The "' + todo.name + '" task has been saved!');
+            // Other db logic would go here
+        };
+    });
