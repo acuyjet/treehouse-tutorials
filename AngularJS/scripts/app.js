4bbf8e7524a27eac4e82f0d8755466e4fcafc98e
acuyjet <allison@cuyjet.com>
2017-02-26 12:53:35 -0500

    Add work from third stage of Angular Js course

--- AngularJS/scripts/app.js
+++ AngularJS/scripts/app.js
@@ -1,21 +1,16 @@
 //Empty array as second arg creates app if it doesn't already exist
 angular.module('todoListApp', [])
     .controller('mainCtrl', function($scope) {
-        $scope.helloWorld = function() {
-            console.log('Hello there! This is the helloWorld controller function in the mainCtrl!');
-        };
-})
-    .controller('coolCtrl', function($scope) {
-        $scope.whoAmI = function() {
-            console.log('Hello there! This is the coolCtrl function!');
+        $scope.learningNgChange = function() {
+            console.log('An input changed!');
         };
 
-        // Overwrites helloWorld() from parent scope because prototypical inheritance only goes in one direction
-        $scope.helloWorld = function() {
-            console.log('This is not the mainCtrl!');
-        }
-})
-    .controller('iAmASibling', function ($scope) {
-        // Do stuff
-        $scope.foobar = 1234;
-    })
+    $scope.todos = [
+        {"name" : "Clean the house"},
+        {"name" : "Water the dog"},
+        {"name" : "Feed the lawn"},
+        {"name" : "Pay dem bills"},
+        {"name" : "Run"},
+        {"name" : "Stretch"}
+    ]
+});
