d472e186f1e55451e7df1468b9e30c6d96857a9f
acuyjet <allison@cuyjet.com>
2017-03-08 20:36:28 -0500

    Add some more Angular services

--- BuildingAMeanApplication/mean-todo/public/scripts/controllers/main.js
+++ BuildingAMeanApplication/mean-todo/public/scripts/controllers/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 angular.module('todoListApp')
-.controller('mainCtrl', function($scope, dataService){
-  
+.controller('mainCtrl', function($scope, $log, $interval,dataService){
+
+    $scope.seconds = 0;
+
+    $scope.counter = function() {
+        $scope.seconds++;
+        $log.log($scope.seconds + ' have passed!'); // Can also use $log.warn or $log.error
+    }
+
+    $interval($scope.counter, 1000, 10);
+
   dataService.getTodos(function(response){
     var todos = response.data.todos;
     $scope.todos =  todos;
