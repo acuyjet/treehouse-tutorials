5450e924feb5df84bb3779fac514799dfe6a8a89
acuyjet <allison@cuyjet.com>
2017-03-11 17:17:07 -0500

    Add some middleware

--- BuildARestApiWithExpress/qa-rest-api/app.js
+++ BuildARestApiWithExpress/qa-rest-api/app.js
@@ -3,6 +3,10 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+    next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
