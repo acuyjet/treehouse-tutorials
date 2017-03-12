a0dcd04757d145ad29d1f049357a8b3138317753
acuyjet <allison@cuyjet.com>
2017-03-12 14:07:24 -0400

    Add logging and error handling; add validation to voting route

--- BuildARestApiWithExpress/qa-rest-api/app.js
+++ BuildARestApiWithExpress/qa-rest-api/app.js
@@ -2,13 +2,32 @@
 
 var express = require('express');
 var app = express();
+var jsonParser = require('body-parser').json;
+var logger = require('morgan');
 var routes = require('./routes');
 
-var jsonParser = require('body-parser').json;
+app.use(logger('dev'));
 
 app.use(jsonParser());
 
 app.use('/questions', routes);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not found!');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler -- extra parameter is how Express knows it's an error handler & not middlware
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 var port = process.env.PORT || 3000;
 
--- BuildARestApiWithExpress/qa-rest-api/routes.js
+++ BuildARestApiWithExpress/qa-rest-api/routes.js
@@ -11,11 +11,11 @@ router.get('/', function(req, res) {
     });
 });
 
-// GET /questions/:id
+// GET /questions/:qID
 // Route for specific questions
-router.get('/:id', function(req, res) {
+router.get('/:qID', function(req, res) {
     res.json({
-        response: 'You sent me a GET request for ID: ' + req.params.id
+        response: 'You sent me a GET request for question ID: ' + req.params.qID
     });
 });
 
@@ -28,6 +28,55 @@ router.post('/', function(req, res) {
     });
 });
 
+// POST /questions/:qID/answers
+// Route for creating an answer
+router.post('/:qID/answers', function(req, res) {
+    res.json({
+        response: 'You sent a POST request to /answers!',
+        questionId: req.params.qID,
+        body: req.body
+    });
+});
 
+// PUT /questions/:qID/answers/:aID
+// Edit a specific answer
+router.put('/:qID/answers/:aID', function(req, res) {
+    res.json({
+        response: 'You sent a PUT request to /answers!',
+        questionId: req.params.qID,
+        answerID: req.params.aID,
+        body: req.body
+    });
+});
+
+// DELETE /questions/:qID/answers/:aID
+// Delete a specific answer
+router.delete('/:qID/answers/:aID', function(req, res) {
+    res.json({
+        response: 'You sent a DELETE request to /answers!',
+        questionId: req.params.qID,
+        answerID: req.params.aID
+    });
+});
+
+// POST /questions/:qID/answers/:aID/vote-up
+// POST /questions/:qID/answers/:aID/vote-down
+// Upvote or downvote a specific answer
+router.post('/:qID/answers/:aID/vote-:direction', function(req, res, next){
+    if(req.params.direction.search(/^(up|down)$/) === -1) {
+        var err = new Error('Not found!');
+        err.status = 404;
+        next(err);
+    } else {
+        next();
+    };
+}, function(req, res) {
+    res.json({
+        response: 'You sent a POST request to /vote-' + req.params.direction,
+        questionId: req.params.qID,
+        answerID: req.params.aID,
+        vote: req.params.direction
+    });
+});
 
 module.exports = router;
