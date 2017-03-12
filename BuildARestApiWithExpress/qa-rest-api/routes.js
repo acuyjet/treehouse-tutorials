9ef6c340f671245fa62e98b8a6c26763b3718654
acuyjet <allison@cuyjet.com>
2017-03-12 17:21:28 -0400

    Edit routes to connect to database

--- BuildARestApiWithExpress/qa-rest-api/routes.js
+++ BuildARestApiWithExpress/qa-rest-api/routes.js
@@ -2,60 +2,88 @@
 
 var express = require('express');
 var router = express.Router();
+var Question = require('./models').Question;
+
+router.param('qID', function(req, res, next, id, err) {
+    Question.findById(id, function(err, doc){
+        if(err) return next(err);
+        if(!doc) {
+            err = new Error('Not found!');
+            err.status = 404;
+            return next(err);
+        }
+        req.question = doc;
+        return next();
+    });
+});
+
+router.param('aID', function(req, res, next, id, err) {
+    req.answer = req.question.answers.id(id);
+    if(!req.answer) {
+        err = new Error('Not found!');
+        err.status = 404;
+        return next(err);
+    }
+    next();
+});
+
 
 // GET /questions
 // Route for questions collection
-router.get('/', function(req, res) {
-    res.json({
-        response: 'You sent me a GET request!'
+router.get('/', function(req, res, next) {
+    Question.find({})
+        .sort({createdAt: -1})
+        .exec(function(err, questions) {
+            if(err) return next(err);
+            res.json(questions);
     });
 });
 
 // GET /questions/:qID
 // Route for specific questions
-router.get('/:qID', function(req, res) {
-    res.json({
-        response: 'You sent me a GET request for question ID: ' + req.params.qID
-    });
+router.get('/:qID', function(req, res, next) {
+    res.json(req.question);
 });
 
 // POST /questions
 // Route for creating questions
-router.post('/', function(req, res) {
-    res.json({
-        response: 'You sent me a POST request!',
-        body: req.body
+router.post('/', function(req, res, next) {
+    var question = new Question(req.body);
+    question.save(function(err, question) {
+        if(err) return next(err);
+        res.status(201);
+        res.json(question);
     });
 });
 
 // POST /questions/:qID/answers
 // Route for creating an answer
-router.post('/:qID/answers', function(req, res) {
-    res.json({
-        response: 'You sent a POST request to /answers!',
-        questionId: req.params.qID,
-        body: req.body
+router.post('/:qID/answers', function(req, res, next) {
+    req.question.answers.push(req.body);
+    req.question.save(function(err, question) {
+        if(err) return next(err);
+        res.status(201);
+        res.json(question);
     });
 });
 
 // PUT /questions/:qID/answers/:aID
 // Edit a specific answer
-router.put('/:qID/answers/:aID', function(req, res) {
-    res.json({
-        response: 'You sent a PUT request to /answers!',
-        questionId: req.params.qID,
-        answerID: req.params.aID,
-        body: req.body
+router.put('/:qID/answers/:aID', function(req, res, next) {
+    req.answer.update(req.body, function(err, result) {
+        if(err) return next(err);
+        res.json(result);
     });
 });
 
 // DELETE /questions/:qID/answers/:aID
 // Delete a specific answer
-router.delete('/:qID/answers/:aID', function(req, res) {
-    res.json({
-        response: 'You sent a DELETE request to /answers!',
-        questionId: req.params.qID,
-        answerID: req.params.aID
+router.delete('/:qID/answers/:aID', function(req, res, next) {
+    req.answer.remove(function(err){
+        req.question.save(function(err, question){
+            if(err) return next(err);
+            res.json(question);
+        });
     });
 });
 
@@ -68,15 +96,14 @@ router.post('/:qID/answers/:aID/vote-:direction', function(req, res, next){
         err.status = 404;
         next(err);
     } else {
+        req.votes = req.params.direction;
         next();
     };
-}, function(req, res) {
-    res.json({
-        response: 'You sent a POST request to /vote-' + req.params.direction,
-        questionId: req.params.qID,
-        answerID: req.params.aID,
-        vote: req.params.direction
-    });
+}, function(req, res, next) {
+        req.answer.vote(req.vote, function(err, question){
+            if(err) return next(err);
+            res.json(question);
+        });
 });
 
 module.exports = router;
