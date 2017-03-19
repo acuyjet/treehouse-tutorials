dc53114f2a5e33671ed5cf046c796651b247fb1d
acuyjet <allison@cuyjet.com>
2017-03-19 16:30:46 -0400

    Add more middleware

--- UserAuthenticationWithExpressAndMongo/routes/index.js
+++ UserAuthenticationWithExpressAndMongo/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var mid = require('../middleware');
 
 // GET /
 router.get('/', function(req, res, next) {
@@ -8,16 +9,16 @@ router.get('/', function(req, res, next) {
 });
 
 // GET /register
-router.get('/register', function(req, res, next) {
+router.get('/register', mid.loggedOut, function(req, res, next) {
     return res.render('register', {title: 'Sign Up'});
 });
 
 // GET /login
-router.get('/login', function(req, res, next) {
+router.get('/login', mid.loggedOut, function(req, res, next) {
     return res.render('login', {title: 'Log In'})
 });
 
-// GET logout
+// GET /logout
 router.get('/logout', function(req, res, next) {
     if(req.session) {
         // Delete session object
@@ -32,12 +33,7 @@ router.get('/logout', function(req, res, next) {
 });
 
 // GET /profile
-router.get('/profile', function(req, res, next) {
-   if (!req.session.userId) {
-       var err = new Error('You are not authorized to view this page!');
-       err.status = 403;
-       return next(err);
-   }
+router.get('/profile', mid.requiresLogin, function(req, res, next) {
    User.findById(req.session.userId)
        .exec(function(error, user) {
            if(error) {
