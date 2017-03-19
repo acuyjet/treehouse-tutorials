530682bc9cbde7acf7e660d7f27df7949db238d8
acuyjet <allison@cuyjet.com>
2017-03-19 18:33:22 -0400

    Add more expectations and refine titleCase function

--- JavaScriptUnitTesting/textUtilities.js
+++ JavaScriptUnitTesting/textUtilities.js
@@ -1,11 +1,14 @@
-// var chai = require('chai');
-// var expect = chai.expect;
 var expect = require('chai').expect;
 
-expect(true).to.be.true;
-
 function titleCase(title){
-    return title;
+    var words = title.split(' ');
+    var titleCasedWords = words.map(function(word){
+        return word[0].toUpperCase() + word.substring(1);
+    });
+    return titleCasedWords.join(' ');
 }
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo')).to.equal('Vertigo');
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
