fe8b8c2587347b809c74481e3c71dea96eed1978
acuyjet <allison@cuyjet.com>
2017-03-25 13:21:04 -0400

    Add stubbed function

--- JavaScriptUnitTesting/game_logic/game_instance.js
+++ JavaScriptUnitTesting/game_logic/game_instance.js
@@ -1,5 +1,16 @@
+var fire = require('./ship_methods.js').fire;
+
 function checkGameStatus(players) {
 	return false;
 }
 
+function takeTurn (opposingPlayer, guessFunction) {
+	var coordinates = guessFunction();
+	fire(opposingPlayer, coordinates);
+	var gameOver = checkGameStatus();
+
+	return gameOver;
+}
+
 module.exports.checkGameStatus = checkGameStatus;
+module.exports.takeTurn = takeTurn;
--- JavaScriptUnitTesting/test/games_test.js
+++ JavaScriptUnitTesting/test/games_test.js
@@ -2,8 +2,40 @@ var expect = require('chai').expect;
 
 describe('GAME INSTANCE FUNCTIONS', function() {
 	describe('checkGameStatus', function() {
+		var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
 		it('Should tell me when the game is over'), function() {
-
+			var players = [
+				{
+					ships: [
+						{
+							locations:[[0, 0]],
+							damage: [[0, 0]]
+						}
+					]
+				}
+			];
+			var acual = checkGameStatus(players);
+			expect(actual).to.be.false;
 		};
 	});
+
+	describe('takeTurn', function() {
+		var takeTurn = require('../game_logic/game_instance').takeTurn;
+		var guess;
+
+		beforeEach(function() {
+			guess = function() { return [0, 0] }
+			player = {ships: [
+				{
+					locations: [[0, 0]],
+					damage: []
+				}
+			]}
+		});
+
+		it('Should return false if the game ends', function() {
+			var actual = takeTurn(player, guess);
+			expect(actual).to.be.false;
+		})
+	})
 });
