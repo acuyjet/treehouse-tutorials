d5f1ee1af5a13bfb152b298605a22e215cfc188e
acuyjet <allison@cuyjet.com>
2020-05-25 20:32:36 -0400

    Add work from Python lists course

---
+++ PythonLists/meeting.py
@@ -0,0 +1,6 @@
+attendees = ["Ken", "Lana", "Treasure"]
+attendees.append("Ashley")
+attendees.extend(["James", "Guil"])
+optional_invitees = ["Ben J.", "Dave"]
+potential_attendees = attendees + optional_invitees
+print("There are", len(potential_attendees), "potential attendees currently.")
---
+++ PythonLists/wishlist.py
@@ -0,0 +1,10 @@
+books = [
+    "Learning Python: Powerful Object-Oriented Programming - Mark Lutz",
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis - Wes McKinney",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
+
+print("Suggested gift: {}".format(books[0]))
