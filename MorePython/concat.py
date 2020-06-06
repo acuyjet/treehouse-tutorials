78e3611e2c0a80210cabeb537684bd144fe252e0
acuyjet <allison@cuyjet.com>
2020-06-06 13:44:25 -0400

    Finish Python Sequences course

---
+++ MorePython/concat.py
@@ -0,0 +1,6 @@
+object1 = "1, 2, 3, 4, 5"
+object2 = "6, 7, 8, 9, 10"
+
+object1 = object1 + object2
+
+print(object1)
---
+++ MorePython/slices.py
@@ -0,0 +1,7 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+nums_partial = nums[0::-1]
+print(nums_partial)
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colors_partial = colors[-1:]
+print(colors_partial)
