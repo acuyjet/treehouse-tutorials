92faf421c39646192d693538d0f4632b58dfcfb9
acuyjet <allison@cuyjet.com>
2020-06-07 17:38:11 -0400

    Add some more work from Object-Oriented Python course

--- ObjectOrientedPython/characters.py
+++ ObjectOrientedPython/characters.py
@@ -1,14 +1,21 @@
 import random
 
 
-class AccordionThief:
-
-	def __init__(self, name, sneaky=True, **kwargs):
+class Character:
+	def __init__(self, name, **kwargs):
 		self.name = name
-		self.sneaky = sneaky
 
 		for key, value in kwargs.items():
 			setattr(self, key, value)
+
+
+class AccordionThief(Character):
+
+	sneaky = True
+
+	def __init__(self, name, sneaky=True, **kwargs):
+		super().__init__(name, **kwargs)
+		self.sneaky = sneaky
 
 	def pickpocket(self):
 		return self.sneaky and bool(random.randint(0,1))
